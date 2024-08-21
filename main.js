9f88cea86a828ba63b173092cfa05d6ee11c48f2
Adam Leis <aleis@iti4dmv.com>
2024-08-21 16:08:19 -0400

    fix column sorting & tweak styles

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ fetch('./GreenEggsAndHam.txt')
 /** @param {String} content */
 function handleText(content) {
     const lines = content
+        .toUpperCase()
         .replaceAll('!', ' ')
         .replaceAll('?', ' ')
         .replaceAll('.', ' ')
@@ -13,11 +14,13 @@ function handleText(content) {
         .replaceAll('(', ' ')
         .replaceAll(')', ' ')
         .replaceAll('\r', '')
+        .replaceAll(',', '\n')
         .split('\n')
         .slice(1)
         .filter(Boolean)
         .map(line => line.trim());
 
+    /** @type {Map<string, number>} wordMap */
     const wordMap = new Map(); // [ word, count ]
 
     for (const line of lines) {
@@ -41,21 +44,53 @@ function handleText(content) {
 
     const ascClass = 'sort--asc';
     const descClass = 'sort--desc';
+    /** @type {number} order -1 = desc, 0 = none, 1 = asc */
+    let order = 0;
+    const orderNext = {
+        // current order: next order
+        '0': 1,
+        '1': -1,
+        '-1': 0,
+    }
+
+    renderRows();
 
     sortableTh.addEventListener('click', () => {
-        if (sortableTh.classList.contains(ascClass)) {
-            sortableTh.classList.remove(ascClass);
-            sortableTh.classList.add(descClass);
-        } else if (sortableTh.classList.contains(descClass)) {
-            sortableTh.classList.remove(descClass);
-        } else {
-            sortableTh.classList.add(ascClass);
-        }
+        order = orderNext[order];
+        handleSortClass();
+        renderRows();
     });
 
-    // TODO sort by value into array ASC/DESC
-    // TODO get colors from book for CSS
-    // TODO display results in table w/ sorting feature ASC/DESC
+    function handleSortClass() {
+        // Clean start
+        sortableTh.classList.contains(ascClass)
+            && sortableTh.classList.remove(ascClass);
+        sortableTh.classList.contains(descClass)
+            && sortableTh.classList.remove(descClass);
+
+        if (order === 1)
+            sortableTh.classList.add(ascClass);
+        if (order === -1)
+            sortableTh.classList.add(descClass);
+    }
+
+    function renderRows() {
+        /** @type {[string, number][]} wordMap */
+        let wordList = Array.from(wordMap);
+        if (order) {
+            // I need to handle this sort much better; JS sort sucks when DESC for some reason
+            wordList = wordList.sort((a, b) => a[1] - b[1]);
+            if (order === -1)
+                wordList = wordList.reverse();
+        }
+
+        let rows = '';
+        for (const [word, count] of wordList) {
+            rows += `<tr><td>${word}</td><td>${count}</td></tr>`;
+        }
+
+        document.querySelector('tbody').innerHTML = rows;
+    }
     // TODO highlight high/med/lowest words used & maybe "Sam-I-Am" count
     // TODO consider tabs with different visualizations like heat map of words frequency or scatter plot graph
 }
